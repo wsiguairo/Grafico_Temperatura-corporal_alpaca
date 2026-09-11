98e8228c8b0336b34c13ac66524b25fe8fcbae23
wsiguairo <wsiguairo@gmail.com>
2026-09-10 21:37:48 -0500

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -31,7 +31,7 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-# AGREGA SOLO ESTA LÍNEA AQUÍ ABAJO:
+# VISUALIZA EN LA PESTAÑA EL TITULO
 st.markdown("<script>window.parent.document.title = 'Gráfica Temperatura Corporal y Clima';</script>", unsafe_allow_html=True)
 
 # ESTILOS - LOGO SENAMHI + BOTONES + TÍTULO PEQUEÑO
